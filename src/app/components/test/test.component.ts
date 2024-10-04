59e07bb4a77f5e7bbd8a89cda7dca3d6f8947edb
zhen <zhen.yang@hotmail.fr>
2024-10-04 20:19:01 +0200

    test material icons

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-test',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './test.component.html',
   styleUrl: './test.component.scss'
 })
